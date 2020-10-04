51d22b1b800635d3836c1bdb1c87fa5fcd2db296
rsaito1216 <mr88888888sr@gmail.com>
2020-10-04 18:24:41 +0900

    Revert "アソシエーションとバリデーションの設定"
    This reverts commit 25d08b0d2cd62d77c39bdedc769f5d39c21e4698.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,21 +4,4 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :items
-  has_many :item_transactions
-
-  with_options presence: true do
-    validates :email, uniqueness: true, format: { with: /\A[a-zA-Z0-9]+\z[@]\A[a-zA-Z0-9]+\z/i}
-    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z{6,}/i}
-  end
-
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/} do
-    validates :last_name
-    validates :first_name
-  end
-
-  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/} do
-    validates :last_name_kana
-    validates :first_name_kana
-  end
 end
