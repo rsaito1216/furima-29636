abf9198387da9d101a6945d38518adca93d5ab1f
rsaito1216 <mr88888888sr@gmail.com>
2020-10-12 14:40:33 +0900

    コントローラーとルーティング

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,33 @@
+class ItemsController < ApplicationController
+  before_action :login_check, only: [:new]
+  
+  def index
+  end
+  
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.valid?
+      @item.save
+      redirect_to action: :index
+    else
+      render action: :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :product_name, :description, :category_id, :condition_id, :delivery_burden_id, :shipping_address_id, :shipping_day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def login_check
+    unless user_signed_in?
+      flash[:alert] = "出品するにはログインか新規登録をしてください"
+      redirect_to new_user_session_path
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "users#index"
-  resources :users, only: [:index, :new, :create]
-  
+  root to: "items#index"
+  resources :users, only: [:new, :create]
+  resources :items, only: [:index, :new, :create]
 end
