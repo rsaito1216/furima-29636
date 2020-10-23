d816c5501526a5cdc7ae9de3f23d06b17ebbc89e
rsaito1216 <mr88888888sr@gmail.com>
2020-10-24 07:49:35 +0900

    商品価格の手数料小数点切り捨て

--- app/javascript/packs/calculation.js
+++ app/javascript/packs/calculation.js
@@ -5,9 +5,9 @@ function calculation (){
     const add = val / 10
     const profit_price = val - add
     const Taxval  = document.getElementById("add-tax-price");
-    Taxval.innerHTML = `${add}`;
+    Taxval.innerHTML = `${Math.floor(add)}`;
     const profit_calculation  = document.getElementById("profit");
-    profit_calculation.innerHTML = `${profit_price}`
+    profit_calculation.innerHTML = `${Math.floor(profit_price)}`
   });
 }
 window.addEventListener('load', calculation);
