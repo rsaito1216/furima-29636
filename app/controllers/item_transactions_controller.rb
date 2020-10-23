e9dfcaab4c621c86867809148641e9883079fe05
rsaito1216 <mr88888888sr@gmail.com>
2020-10-23 11:49:29 +0900

    不要の行を削除

--- app/controllers/item_transactions_controller.rb
+++ app/controllers/item_transactions_controller.rb
@@ -27,7 +27,6 @@ class ItemTransactionsController < ApplicationController
   end
 
   def pay_item
-    @item = Item.find(params[:item_id])
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
       amount: @item.price,
