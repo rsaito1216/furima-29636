cc1a1af85fcd2bfc19906999e4f387b294b6980e
rsaito1216 <mr88888888sr@gmail.com>
2020-10-29 22:12:22 +0900

    コメントのみAction Cableの実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,9 @@
 class CommentsController < ApplicationController
   def create
     comment = Comment.create(comment_params)
+    if comment.save
+      ActionCable.server.broadcast 'comment_channel', content: comment
+    end
   end
 
   private
