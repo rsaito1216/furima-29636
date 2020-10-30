5d4916ee7aad7a9a3ed0aed03632cca6bacb94b1
rsaito1216 <mr88888888sr@gmail.com>
2020-10-31 01:17:14 +0900

    JavaScriptや表示の記述を色々変更

--- app/javascript/packs/calculation.js
+++ app/javascript/packs/calculation.js
@@ -1,13 +1,24 @@
-function calculation (){
-  const price  = document.getElementById("item-price");
-  price.addEventListener("keyup", () => {
-    const val = price.value
-    const add = val / 10
-    const profit_price = val - add
-    const Taxval  = document.getElementById("add-tax-price");
-    Taxval.innerHTML = `${Math.floor(add)}`;
-    const profit_calculation  = document.getElementById("profit");
-    profit_calculation.innerHTML = `${Math.floor(profit_price)}`
-  });
+
+
+if (document.URL.match("/new") || document.URL.match("/edit")) {
+  function calculation (){
+    const price  = document.getElementById("item-price");
+    price.addEventListener("keyup", () => {
+      const val = price.value
+      const add = val / 10
+      const profit_price = val - add
+      
+      // カンマ表示の実験
+      var num = add;
+      var after = String(num).replace( /(\d)(?=(\d\d\d)+(?!\d))/g, '$1,' );
+      console.log(after);
+      // カンマ表示の実験
+
+      const Taxval  = document.getElementById("add-tax-price");
+      Taxval.innerHTML = Math.floor(add);
+      const profit_calculation  = document.getElementById("profit");
+      profit_calculation.innerHTML = Math.floor(profit_price);
+    });
+  }
+  window.addEventListener('load', calculation);
 }
-window.addEventListener('load', calculation);
--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,32 +1,34 @@
-document.addEventListener('DOMContentLoaded', function(){
-  const ImageList = document.getElementById('image-list');
-  
-  const createImageHTML = (blob) => {
-    // 画像を表示するためのdiv要素を生成
-   const imageElement = document.createElement('div');
-   imageElement.classList.add( "preview-image" ) ;
-   // 表示する画像を生成
-   const blobImage = document.createElement('img');
-   
-   blobImage.setAttribute('src', blob );
-   blobImage.style.width = "250px";
-   blobImage.style.height = "200px";
-   
-   // 生成したHTMLの要素をブラウザに表示させる
-   imageElement.appendChild(blobImage);
-   
-   ImageList.appendChild(imageElement);
- };
-  
-  document.getElementById('item-image').addEventListener('change', function(e){
-    const imageContent = document.querySelector('img');
-      if (imageContent){
-        imageContent.remove();
-      }
+if (document.URL.match("/new") || document.URL.match("/edit")) {
+  document.addEventListener('DOMContentLoaded', function(){
+    const ImageList = document.getElementById('image-list');
+    
+    const createImageHTML = (blob) => {
+      // 画像を表示するためのdiv要素を生成
+    const imageElement = document.createElement('div');
+    imageElement.classList.add( "preview-image" ) ;
+    // 表示する画像を生成
+    const blobImage = document.createElement('img');
+    
+    blobImage.setAttribute('src', blob );
+    blobImage.style.width = "250px";
+    blobImage.style.height = "200px";
+    
+    // 生成したHTMLの要素をブラウザに表示させる
+    imageElement.appendChild(blobImage);
+    
+    ImageList.appendChild(imageElement);
+  };
+    
+    document.getElementById('item-image').addEventListener('change', function(e){
+      const imageContent = document.querySelector('img');
+        if (imageContent){
+          imageContent.remove();
+        }
 
-    const file = e.target.files[0];
-    const blob = window.URL.createObjectURL(file);
+      const file = e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
 
-    createImageHTML(blob);
+      createImageHTML(blob);
+    });
   });
-});
+}
