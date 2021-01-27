66509ab140e06e4552c8d02b03972e397cd06195
rsaito1216 <mr88888888sr@gmail.com>
2021-01-27 11:39:47 +0900

    ユーザーメニュー再度修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,7 @@ require("channels")
 require('./calculation')
 require("../card")
 require('./preview.js')
+require('./pulldown.js')
 require('jquery')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
---
+++ app/javascript/packs/pulldown.js
@@ -0,0 +1,31 @@
+function pullDown() {
+
+  const pullDownButton = document.getElementById("lists")
+  const pullDownParents = document.getElementById("pull-down")
+  const pullDownChild = document.querySelectorAll(".pull-down-list")
+
+  pullDownButton.addEventListener('mouseover', function(){
+    this.setAttribute("style", "background-color:rgb(195, 153, 207);")
+  })
+
+  pullDownButton.addEventListener('mouseout', function(){
+    this.removeAttribute("style", "background-color:rgb(195, 153, 207);")
+  })
+
+  pullDownButton.addEventListener('click', function() {
+    if (pullDownParents.getAttribute("style") == "display:block;") {
+      pullDownParents.removeAttribute("style", "display:block;")
+    } else {
+      pullDownParents.setAttribute("style", "display:block;")
+    }
+  })
+
+  pullDownChild.forEach(function(list) {
+    list.addEventListener('click', function() {
+      value = list.innerHTML
+      currentList.innerHTML = value
+    })
+  })
+}
+
+window.addEventListener('load', pullDown)
